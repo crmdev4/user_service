110972e0fd4a7c1c56e1008f9f032adb213a8bf4
aswanesher <aswansetiawan01@gmail.com>
2025-03-19 22:42:18 +0700

    displace verify to route web

--- routes/web.php
+++ routes/web.php
@@ -7,16 +7,17 @@ use App\Http\Controllers\Web\RelayController;
 use App\Http\Controllers\API\EmployeeVerificationController;
 
 Route::get('/', function () {
-    return response()->json(['message' => 'OKE'], 200); });
+    return response()->json(['message' => 'OKE'], 200);
+});
 
 Route::prefix('/dashboard/relay')->group(function () {
     Route::get('/list', [RelayController::class, 'index']);
     Route::get('/relation/{id}', [RelayController::class, 'getRelation']);
 });
 
-Route::get('/verify-email', [EmployeeVerificationController::class, 'verify'])->name('verify.email');
-Route::get('/verify-email/{token}', [EmployeeVerificationController::class, 'verifyToken'])->name('verify.email.token');
+// Route::get('/verify-email', [EmployeeVerificationController::class, 'verify'])->name('verify.email');
+Route::get('/verify-email/{token}', [UserVerificationController::class, 'verify']);
+// Route::get('/verify-email/{token}', [EmployeeVerificationController::class, 'verifyToken'])->name('verify.email.token');
 
 Route::get('/forgot-password/{token}', [EmailForgotPasswordController::class, 'verify'])->name('verify.email-forgot-password');
 Route::get('/verify-user-email', [UserVerificationController::class, 'verifyUser'])->name('verify.user.email');
-
